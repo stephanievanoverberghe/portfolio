8d5456480e6853ada7df10df073dfcc0be3ffb0f
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-06-08 17:18:48 +0200

    add: exist

---
+++ README.md
@@ -0,0 +1,38 @@
+# PORTFOLIO - Vanoverberghe Stéphanie - Développeuse Front-End spécialisée en React
+
+[![npm version](https://badge.fury.io/js/npm.svg)](https://badge.fury.io/js/npm) ![GitHub last commit](https://img.shields.io/github/last-commit/stephanievanoverberghe/sv-portfolio)
+
+## Table des matières
+
+- [PORTFOLIO - Vanoverberghe Stéphanie - Développeuse Front-End spécialisée en React](#portfolio---vanoverberghe-stéphanie---développeuse-front-end-spécialisée-en-react)
+  - [Table des matières](#table-des-matières)
+  - [Aperçu](#aperçu)
+    - [Description projet](#description-projet)
+    - [Fonctionnalités](#fonctionnalités)
+    - [Screenshots](#screenshots)
+    - [Liens](#liens)
+  - [Contact](#contact)
+
+## Aperçu
+
+### Description projet
+
+**Technologies utilisées**
+
+![Tailwind CSS](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![JSON](https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white)
+
+![Vite](https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E)
+![NPM](https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white)
+
+### Fonctionnalités
+
+### Screenshots
+
+### Liens
+
+- Code du site : [GitHub](https://github.com/stephanievanoverberghe/sv-portfolio)
+- Site en direct : [GitHub Pages](https://stephanievanoverberghe.github.io/sv-portfolio/)
+
+## Contact
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,52 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js, jsx}"],
+  content: [
+    "./src/**/*.{html, js}",
+    "./*.{html, js}",
+    "./pages/**/*.{html, js}",
+    "./pages/*.{html, js}"
+  ],
   theme: {
     // A l'extérieur, ça remplace les fonctionnalités se trouvant dans le thème tailwind
     extend: {
       // A l'intérieur, ça ajoute des fonctionnalités au thème tailwind sans rien écraser
+      colors: {
+        'red-pink': {
+          50: '#fff1f3',
+          100: '#ffe4e7',
+          200: '#ffccd4',
+          300: '#fea3b1',
+          400: '#fd6f88',
+          500: '#f73c63',
+          600: '#d31848',
+          700: '#c10f42',
+          800: '#a1103d',
+          900: '#8a113b',
+          950: '#4d041c',
+          1000: '#2C000F'
+        }
+      },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        Patua: ['Patua One', 'serif'],
+      },
+      boxShadow: {
+        'dark-hover': 'inset -2px -2px 6px 0 #fd6f88, inset 2px 2px 6px 0 #2C000F, -2px -2px 6px 0 #fd6f88, 2px 2px 6px 0 #2C000F',
+        'dark-default': '-2px -2px 6px 0 #fd6f88, 5px 5px 4px 0 #2C000F',
+        'dark-pressed': 'inset -2px -2px 4px 0 #fd6f88, inset 3px 3px 4px 0 #2C000F',
+        'light-hover': 'inset -4px -4px 4px 0 #fff, inset 2px 2px 6px 0 #D4C8CA, -2px -2px 6px 0 #fff, 2px 2px 6px 0 #D4C8CA',
+        'light-default': ' -2px -2px 6px 0 #fff, 2px 2px 6px 0 #D4C8CA',
+        'light-pressed': 'inset -2px -2px 4px 0 #fff, inset 2px 2px 6px 0 #D4C8CA',
+        'shadow-header': '0 -2px 4px 0 #D4C8CA',
+        'shadow-hero-dark': '-2px -2px 6px 0 #f73c63, 5px 5px 4px 0 #1C000F'
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
 }
 
